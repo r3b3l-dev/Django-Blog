82f14a097a221340c461b861c9a2ab950a90060f
r3b3l-dev <wayne.manning@tuta.io>
2022-04-08 16:08:26 +0000

    Catch up 3

--- blog/admin.py
+++ blog/admin.py
@@ -5,21 +5,20 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    
+
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
-    search_fields = ['name', 'email', 'body']
+    search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
-    
+
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
 from . import views
+from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
