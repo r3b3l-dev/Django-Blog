b5e775878c63e7cb8ea5c33b657e1cc0c235a71f
r3b3l-dev <wayne.manning@tuta.io>
2022-04-09 15:38:33 +0000

    deployment commit

--- blog/views.py
+++ blog/views.py
@@ -68,8 +68,9 @@ class PostDetail(View):
 
 class PostLike(View):
     
-    def post(self, request, slug, *args, **kwargs):
+    def post(self, request, slug):
         post = get_object_or_404(Post, slug=slug)
+        
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
 from pathlib import Path
-
+import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['codestar-r3b3l.herokuapp.com', 'localhost']
 
@@ -57,6 +57,14 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
